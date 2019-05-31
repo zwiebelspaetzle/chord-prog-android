7577670117f94fdcf80b3e110dd7769b72391ac7
Chris Ward <cward@ddm.io>
2019-05-31 15:06:31 -0600

    set samples by key

--- app/src/main/java/com/example/chordprogressions/MainActivity.kt
+++ app/src/main/java/com/example/chordprogressions/MainActivity.kt
@@ -12,64 +12,74 @@ import android.os.Build
 
 
 class MainActivity : AppCompatActivity() {
-
+    private val semitones = arrayOf("c", "cs", "d", "ds", "e", "f", "fs", "g", "gs", "a", "as", "b")
     private lateinit var soundPool: SoundPool
-    private var sample1: Int = 0
-    private var sample2: Int = 0
-    private var sample3: Int = 0
-    private var sample4: Int = 0
-    private var sample5: Int = 0
-    private var sample6: Int = 0
-    private var sample7: Int = 0
+    private var currentKey = "c"
+    private var sampleArray = IntArray(7)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            val audioAttributes = AudioAttributes.Builder()
-                .setUsage(AudioAttributes.USAGE_GAME)
-                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-                .setFlags(AudioAttributes.FLAG_LOW_LATENCY)
-                .build()
 
-            soundPool = SoundPool.Builder()
-                .setMaxStreams(7)
-                .setAudioAttributes(audioAttributes)
-                .build()
-//        } else {
-//            soundPool = SoundPool(7, AudioManager.STREAM_MUSIC, 0)
-//        }
+        val audioAttributes = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_GAME)
+            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+            .setFlags(AudioAttributes.FLAG_LOW_LATENCY)
+            .build()
 
-        sample1 = soundPool.load(this, R.raw.db_d_48k, 1)
-        sample2 = soundPool.load(this, R.raw.db_e_48k, 1)
-        sample3 = soundPool.load(this, R.raw.db_fs_48k, 1)
-        sample4 = soundPool.load(this, R.raw.db_g_48k, 1)
-        sample5 = soundPool.load(this, R.raw.db_a_48k, 1)
-        sample6 = soundPool.load(this, R.raw.db_b_48k, 1)
-        sample7 = soundPool.load(this, R.raw.db_cs_48k, 1)
-    }
+        soundPool = SoundPool.Builder()
+            .setMaxStreams(7)
+            .setAudioAttributes(audioAttributes)
+            .build()
 
-    fun playSample(v: View) {
-        when (v.id) {
-            R.id.s1button -> soundPool.play(sample1, 1.toFloat(), 1.toFloat(), 0, 0, 1.toFloat())
-            R.id.s2button -> soundPool.play(sample2, 1.toFloat(), 1.toFloat(), 0, 0, 1.toFloat())
-            R.id.s3button -> soundPool.play(sample3, 1.toFloat(), 1.toFloat(), 0, 0, 1.toFloat())
-            R.id.s4button -> soundPool.play(sample4, 1.toFloat(), 1.toFloat(), 0, 0, 1.toFloat())
-            R.id.s5button -> soundPool.play(sample5, 1.toFloat(), 1.toFloat(), 0, 0, 1.toFloat())
-            R.id.s6button -> soundPool.play(sample6, 1.toFloat(), 1.toFloat(), 0, 0, 1.toFloat())
-            R.id.s7button -> soundPool.play(sample7, 1.toFloat(), 1.toFloat(), 0, 0, 1.toFloat())
-        }
+        setSamplesFromKey("d")
     }
 
     override fun onDestroy() {
         super.onDestroy()
         soundPool.release()
-//        soundPool = null
+    }
+
+    private fun setSamplesFromKey(key: String) {
+        val scale = getScale(key)
+        for ((i, note) in scale.withIndex()) {
+            sampleArray[i] = soundPool.load(
+                this,
+                this.resources.getIdentifier("db_"+scale[i]+"_48k","raw", this.packageName),
+                1)
+        }
+    }
+
+    private fun getScale(key: String): Array<String?> {
+        val semitoneSequence: IntArray = intArrayOf(0, 2, 4, 5, 7, 9, 11)
+        val root = semitones.indexOf(key)
+        var scale =  arrayOfNulls<String>(7)
+
+        for ((i, offset) in semitoneSequence.withIndex()) {
+            val index = if (root + offset < semitones.count()) root + offset else root + offset - semitones.count()
+            scale[i] = semitones[index]
+        }
+
+        return scale
+    }
+
+    fun playSample(v: View) {
+        when (v.id) {
+            R.id.s1button -> soundPool.play(sampleArray[0], 1.toFloat(), 1.toFloat(), 0, 0, 1.toFloat())
+            R.id.s2button -> soundPool.play(sampleArray[1], 1.toFloat(), 1.toFloat(), 0, 0, 1.toFloat())
+            R.id.s3button -> soundPool.play(sampleArray[2], 1.toFloat(), 1.toFloat(), 0, 0, 1.toFloat())
+            R.id.s4button -> soundPool.play(sampleArray[3], 1.toFloat(), 1.toFloat(), 0, 0, 1.toFloat())
+            R.id.s5button -> soundPool.play(sampleArray[4], 1.toFloat(), 1.toFloat(), 0, 0, 1.toFloat())
+            R.id.s6button -> soundPool.play(sampleArray[5], 1.toFloat(), 1.toFloat(), 0, 0, 1.toFloat())
+            R.id.s7button -> soundPool.play(sampleArray[6], 1.toFloat(), 1.toFloat(), 0, 0, 1.toFloat())
+        }
     }
 
     fun playWhiskey(view: View) {
         var mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.whiskey)
         mediaPlayer?.start()
     }
+
+
 }
