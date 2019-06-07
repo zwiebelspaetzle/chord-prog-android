159a73f9d6311361a63cdd5dfd34f59dd3419d69
Chris Ward <cward@ddm.io>
2019-06-07 13:38:11 -0600

    key now selectable

--- app/src/main/java/com/example/chordprogressions/MainActivity.kt
+++ app/src/main/java/com/example/chordprogressions/MainActivity.kt
@@ -8,12 +8,17 @@ import android.view.View
 import android.media.AudioManager
 import android.media.AudioAttributes
 import android.os.Build
+import android.util.Log
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+val defaultKey = "C"
 
-
-
-class MainActivity : AppCompatActivity() {
+// adding AdapterView.OnItemSelectedListener to handle spinner in same file
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     private val semitones = arrayOf("c", "cs", "d", "ds", "e", "f", "fs", "g", "gs", "a", "as", "b")
     private lateinit var soundPool: SoundPool
+    private lateinit var keys: Array<String>
     private var currentKey = "c"
     private var sampleArray = IntArray(7)
 
@@ -21,6 +26,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // set up spinner
+        keys = resources.getStringArray(R.array.keys_array)
+        val keySpinner: Spinner = findViewById(R.id.keySpinner)
+        with(keySpinner) {
+            onItemSelectedListener = this@MainActivity
+            setSelection(keys.indexOf(defaultKey))
+        }
 
         val audioAttributes = AudioAttributes.Builder()
             .setUsage(AudioAttributes.USAGE_GAME)
@@ -33,13 +45,26 @@ class MainActivity : AppCompatActivity() {
             .setAudioAttributes(audioAttributes)
             .build()
 
-        setSamplesFromKey("d")
+        // set samples from our selected key
+        setSamplesFromKey(keys[keySpinner.selectedItemPosition].toLowerCase())
     }
 
     override fun onDestroy() {
         super.onDestroy()
         soundPool.release()
     }
+
+    // handle spinner selection
+    override fun onItemSelected(parent: AdapterView<*>?, v: View?, position: Int, id: Long) {
+        Log.d("cp", "Key ${keys[position]} selected")
+        setSamplesFromKey(keys[position])
+    }
+
+    // required for Spinner
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        Log.d("cp","Nothing selected")
+    }
+
 
     private fun setSamplesFromKey(key: String) {
         val scale = getScale(key)
@@ -53,8 +78,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun getScale(key: String): Array<String?> {
         val semitoneSequence: IntArray = intArrayOf(0, 2, 4, 5, 7, 9, 11)
-        val root = semitones.indexOf(key)
-        var scale =  arrayOfNulls<String>(7)
+        val root = semitones.indexOf(getScaleRoot(key))
+        var scale = arrayOfNulls<String>(7)
 
         for ((i, offset) in semitoneSequence.withIndex()) {
             val index = if (root + offset < semitones.count()) root + offset else root + offset - semitones.count()
@@ -62,6 +87,20 @@ class MainActivity : AppCompatActivity() {
         }
 
         return scale
+    }
+
+    // just some translation from human-formatted keys to note file names
+    private fun getScaleRoot(key: String): String {
+        var root = key.toLowerCase()
+        if (key.contains('♭', ignoreCase = true)) {  // change d♭ to cs, etc
+            var rootChar = root[0]
+            rootChar = if (rootChar == 'a') 'g' else rootChar-1
+            root =  rootChar + "s"
+        }
+
+        Log.d("cp", "Key: $key, Root: $root")
+
+        return root
     }
 
     fun playSample(v: View) {
@@ -80,6 +119,4 @@ class MainActivity : AppCompatActivity() {
         var mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.whiskey)
         mediaPlayer?.start()
     }
-
-
 }
